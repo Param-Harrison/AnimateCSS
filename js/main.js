155773aefc640393c63d680055a6836e2573e2dc
Param-Harrison <paramanantham@live.com>
2014-01-24 17:32:30 +0530

    CSS3 Image Gallery

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('.loader').addClass('la-animate');
-	setTimeout(function(){
-		$('.loader').addClass('la-rotate');
-		//$('.loader').removeClass('la-animate');
-	}, 5000);
-});
